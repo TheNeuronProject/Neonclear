ce5b3bcd09701d3965d2fba1480c67c89535b2c6
Yukino Song <syqlds@126.com>
2017-08-01 01:27:55 +0800

    Add modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // Normalize
 import 'normalize.css'
+// Import global style
+import './global.css'
 // Export ef
 export * from 'ef.js'
 // Export every module
 export * from './modules/body'
 export * from './modules/header'
+export * from './modules/text_logo'
