4e02a69ed6022f961adc1a4cf7bd34fa6ff1b142
Yukino Song <syqlds@126.com>
2017-08-02 00:12:42 +0800

    Add modules, update test page

--- src/index.js
+++ src/index.js
@@ -18,9 +18,12 @@ export * from 'ef.js'
 // Export every module
 export * from './modules/body'
 export * from './modules/header'
-export * from './modules/text_logo'
-export * from './modules/page'
 export * from './modules/footer'
+export * from './modules/text_logo'
+export * from './modules/banner_logo'
+export * from './modules/page'
+export * from './modules/logo_page'
+export * from './modules/button'
 
 // Log style
 const ls = `
