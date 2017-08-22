58ed61648e1ed505310ddab6caccdb5309089698
Yukino Song <syqlds@126.com>
2017-08-22 17:49:02 +0800

    Change colorset

--- config/colors.config.js
+++ config/colors.config.js
@@ -1,11 +1,5 @@
-// module.exports = {
-// 	colorLeft: '#b200fb',
-// 	colorRight: '#6f00e8',
-// 	colorShadow: 'rgba(201, 69, 255, 0.5)'
-// }
-
-module.exports = {
-	colorLeft: '#00c3bf',
-	colorRight: '#00b999',
+export default {
+	colorLeft: '#00fffa',
+	colorRight: '#00b9ee',
 	colorShadow: 'rgba(56, 239, 222, 0.5)'
 }
